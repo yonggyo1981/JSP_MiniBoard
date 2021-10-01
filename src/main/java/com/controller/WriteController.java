f391902af736c8b193a719616cc7ca47bc3530e3
YONGGYO LEE <yonggyo1981@gmail.com>
2021-10-01 16:18:39 +0900

    Mini Board

---
+++ src/main/java/com/controller/WriteController.java
@@ -0,0 +1,35 @@
+package com.controller;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+/***
+ * 게시글 작성 컨트롤러(서블릿)
+ * 
+ * GET 접근 - 게시글 작성 양식 (doGet)
+ * POST 접근 - 게시글 등록 처리 (doPost)
+ */
+public class WriteController extends HttpServlet {
+	private static final long serialVersionUID = -1L;
+	
+	/**
+	 * 게시글 양식 출력 
+	 */
+	@Override 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("/board/form.jsp");
+		rd.include(request, response);
+	}
+	
+	/**
+	 * 게시글 저장 처리 
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+}
---
+++ src/main/webapp/board/form.jsp
@@ -0,0 +1,2 @@
+<%@ page contentType="text/html; charset=utf-8" %>
+게시글 작성(수정) 양식
