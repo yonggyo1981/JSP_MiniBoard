1e20dd4376bebd24a0f983f374b739a247072676
YONGGYO LEE <yonggyo1981@gmail.com>
2021-10-05 11:13:42 +0900

    .

---
+++ src/main/java/com/model/dao/BoardDAO.java
@@ -0,0 +1,22 @@
+package com.model.dao;
+
+import javax.servlet.http.HttpServletRequest;
+import com.core.*;
+
+/**
+ * 게시판 DAO
+ *
+ */
+public class BoardDAO {
+	/**
+	 * 게시글 작성 
+	 * 
+	 * @param request
+	 * @return 게시글 작성 성공시 등록된 게시글 번호(0이면 실패...)
+	 */
+	public int write(HttpServletRequest request) {
+		int idx = 0;
+		
+		return idx;
+	}
+}
