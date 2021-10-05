907da6a45cb8890d88f4296efa8d67159715e564
YONGGYO LEE <yonggyo1981@gmail.com>
2021-10-05 15:47:44 +0900

    .

--- src/main/java/com/controller/DeleteController.java
+++ src/main/java/com/controller/DeleteController.java
@@ -5,6 +5,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import com.model.dao.BoardDAO;
 
 public class DeleteController extends HttpServlet {
 	
@@ -12,6 +15,21 @@ public class DeleteController extends HttpServlet {
 	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		if (request.getParameter("idx") == null) {
+			out.print("<script>alert('잘못된 방식으로 접근하셨습니다.');history.back();</script>");
+			return;
+		}
+		
+		BoardDAO dao = new BoardDAO();
+		int idx = Integer.parseInt(request.getParameter("idx"));
+		boolean result = dao.delete(idx);
+		if (result) { // 삭제 성공 -> 목록으로 이동
+			response.sendRedirect("list");
+		} else { // 삭제 실패 -> 메세지 -> 뒤로 가기 
+			out.print("<script>alert('삭제 실패!');history.back();</script>");
+		}
 		
 	}
 }
