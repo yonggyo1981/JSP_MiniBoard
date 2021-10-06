1e764aec3b60be39a2688beaef50b929e6b42a65
YONGGYO LEE <yonggyo1981@gmail.com>
2021-10-06 12:48:00 +0900

    .

--- src/main/java/com/controller/FileUploadController.java
+++ src/main/java/com/controller/FileUploadController.java
@@ -1,7 +1,34 @@
 package com.controller;
 
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import java.io.IOException;
 
+/**
+ * 파일 업로드 처리 
+ *
+ * GET - 업로드 양식 
+ * POST - 업로드 처리 
+ */
 public class FileUploadController extends HttpServlet {
-
+	
+	private static final long serialVersionUID = -1L;
+	
+	/** 업로드 양식 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("/board/upload.jsp");
+		rd.include(request, response);
+	}
+	
+	/** 업로드 처리 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
 }
+
+
+
+
