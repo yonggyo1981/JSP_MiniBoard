4d51b11a70baa2388c1728796c030485a6b54973
YONGGYO LEE <yonggyo1981@gmail.com>
2021-10-06 16:12:58 +0900

    .

--- src/main/java/com/controller/FileUploadController.java
+++ src/main/java/com/controller/FileUploadController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import com.core.FileManager;
 
@@ -27,8 +28,15 @@ public class FileUploadController extends HttpServlet {
 	
 	/** 업로드 처리 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		
 		String uploadedFiles = FileManager.upload(request);
-		System.out.println(uploadedFiles);
+		if (uploadedFiles == null) { // 업로드된 파일이 없는 경우, 이미지 파일이 아닌 경우
+			out.print("<script>alert('업로드 실패!');</script>");
+		} else { // 업로드가 된 경우 
+			out.printf("<script>parent.parent.callbackUploadImages('%s')</script>", uploadedFiles);
+		}
 	}
 }
 
--- src/main/webapp/resources/js/form.js
+++ src/main/webapp/resources/js/form.js
@@ -9,5 +9,18 @@ $(function() {
 
 /** 이미지 업로드 후 처리할 콜백 함수 */
 function callbackUploadImages(images) {
-	console.log(images);	
+	if (!images)
+		return;
+	
+	images = images.split("||");
+	let html = "";
+	images.forEach(function(image) {
+		html += `<img src='${image}'>`;
+	});
+	
+	CKEDITOR.instances.content.insertHtml(html);
+	
+	layer.close();
 }
+
+
